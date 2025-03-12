5b8727f9914f16771698b887fe1fedfd10cabf32
zuhri utama <zuhriutama@gmail.com>
2025-03-12 22:32:05 +0700

    added link pelaporan kematian

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -4,6 +4,7 @@
 <li><a href="{{ route('request.create') }}">Pengajuan</a></li>
 <li><a href="{{ route('arrival.create') }}">Pendatang</a></li>
 <li><a href="{{ route('request.check') }}">Cek Pengajuan</a></li>
+<li><a href="https://bit.ly/LaporanKematianWarga_KebonJeruk">Pelaporan Kematian</a></li>
 @guest
 <li><a href="{{ route('login') }}">Masuk</a></li>
 @else
