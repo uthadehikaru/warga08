18b8419b8bc83ac240c481343d8d20f321d4c22e
zuhri utama <zuhriutama@gmail.com>
2025-05-20 22:47:05 +0700

    update viewport

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=1.0, user-scalable=no">
 
         <title>@yield('title', config('app.name'))</title>
         <link rel="icon" href="{{ asset('posyandu.png') }}" type="image/png">
