83d7da4717bd55c99f0d418e1e8059d1aaadfcdd
zuhri utama <zuhriutama@gmail.com>
2025-06-09 12:57:16 +0700

    add wa button on record

--- resources/views/livewire/posyandu/health-records.blade.php
+++ resources/views/livewire/posyandu/health-records.blade.php
@@ -157,6 +157,10 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                 </svg>
                         </a>
+                        <a href="https://wa.me/{{ $healthHistory->healthRecord->user->phone }}?text={{ $healthHistory->summary }}" target="_blank" class="btn btn-sm btn-success p-1">
+                            whatsapp
+                        </a>
+                        
                         <button wire:click="delete({{ $healthHistory->id }})" onclick="return confirm('Apakah anda yakin ingin menghapus riwayat kesehatan ini?')" class="btn btn-sm btn-error p-1">
                             hapus <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
